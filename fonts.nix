5c5614456cca552cde3124ced031afd491acec7b
b1u3rchie <b1u3b3rrys@proton.me>
2025-10-28 18:36:21 +0100

    Update fonts.nix

--- fonts.nix
+++ fonts.nix
@@ -6,10 +6,5 @@ fonts.packages = with pkgs; [
   noto-fonts-cjk-sans
   noto-fonts-emoji
   liberation_ttf
-  fira-code
-  fira-code-symbols
-  mplus-outline-fonts.githubRelease
-  dina-font
-  proggyfonts
 ];
 }
