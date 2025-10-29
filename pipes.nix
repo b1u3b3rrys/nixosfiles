79a8e943e980d12761bb9dc0aec357e399b274a2
b1u3rchie <b1u3b3rrys@proton.me>
2025-10-29 21:24:20 +0100

    Update pipes.nix

--- pipes.nix
+++ pipes.nix
@@ -8,7 +8,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment the following
     jack.enable = true;
   };
   environment.systemPackages = with pkgs; [
