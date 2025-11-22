65de55f8bf2c79370d025c1b85f32432366160f6
b1u3rchie <b1u3b3rrys@proton.me>
2025-11-22 21:53:57 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,24 @@
+
 {
-  description = "chaotic-repo";
+  description = "chaotic-repo and home-manager";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; # IMPORTANT
+    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; 
+home-manager = {
+     url = "github:nix-community/home-manager";
+     inputs.nixpkgs.follows = "nixpkgs";
+   };
   };
 
-  outputs = { nixpkgs, chaotic, ... }: {
+  outputs = { nixpkgs, chaotic, home-manager, ... }: {
     nixosConfigurations = {
       nixOS = nixpkgs.lib.nixosSystem { # Replace "nixOS" with your system's hostname 
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          chaotic.nixosModules.default # IMPORTANT
+          chaotic.nixosModules.default
+          home-manager.nixosModules.home-manager
         ];
       };
     };
