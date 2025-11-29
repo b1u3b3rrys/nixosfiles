7cd9709ec7042d44c5845694817bc4f3d10a4cf1
b1u3rchie <b1u3b3rrys@proton.me>
2025-11-29 23:22:48 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -4,20 +4,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    chaotic.url = "github:chaotic-cx/nyx/nyxpkgs-unstable"; 
 home-manager = {
      url = "github:nix-community/home-manager";
      inputs.nixpkgs.follows = "nixpkgs";
    };
   };
 
-  outputs = { nixpkgs, chaotic, home-manager, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       nixOS = nixpkgs.lib.nixosSystem { # Replace "nixOS" with your system's hostname 
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
-          chaotic.nixosModules.default
           home-manager.nixosModules.home-manager
         ];
       };
