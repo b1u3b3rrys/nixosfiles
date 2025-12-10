f0c24eca19411d9e2ab331ab5841973aa6d65fc8
b1u3rchie <b1u3b3rrys@proton.me>
2025-12-10 22:02:27 +0100

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -2,35 +2,45 @@
 
 {
 
-  home.stateVersion = "25.05";
-dconf = {
-    settings = {
-        "org/cinnamon/desktop/applications/terminal" = {
-            exec = "alacritty";
-            # exec-arg = ""; # argument
-        };
-    };
-};
-  xdg.desktopEntries.nemo = {
-    name = "Nemo";
-    exec = "${pkgs.nemo}/bin/nemo";
-};
-  xdg.mimeApps = {
-    enable = true;
-    defaultApplications = {
-        "inode/directory" = [ "nemo.desktop" ];
-        "application/x-gnome-saved-search" = [ "nemo.desktop" ];
-    };
-};
-
-  home.packages = [];
+#imports = [
+  #./niri.nix
+#];
 
 
-  };
-  home.sessionVariables = {
-    EDITOR = "micro";
-  };
+#xdg.configFile."niri/config.kdl".force = true;
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "b1u3rchie";
+  home.homeDirectory = "/home/b1u3rchie";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "26.05";
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  
+  gtk = {
+    enable = true;
+ };
+    
+ home.pointerCursor = {
+    name = "BreezeX-RosePine-Linux";
+    package = pkgs.rose-pine-cursor;
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
+  home.sessionVariables = {
+    XCURSOR_THEME = "BreezeX-RosePine-Linux";
+    XCURSOR_SIZE = "24";
+    QT_QPA_PLATFORMTHEME = "gtk3";
+  };
 }
