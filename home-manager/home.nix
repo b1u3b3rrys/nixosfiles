240b7476df3a86da7d48000cc2447b0b3c1b18be
b1u3rchie <b1u3b3rrys@proton.me>
2025-12-13 16:28:44 +0000

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,10 +1,12 @@
+{ config, pkgs, ... }:
 {
+
 
 #imports = [
   #./niri.nix
 #];
 
-
+xdg.configFile."gtk-4.0/settings.ini".force = true;
 #xdg.configFile."niri/config.kdl".force = true;
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
@@ -13,6 +15,56 @@
 
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "26.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  
+gtk = {
+  enable = true;
+
+  iconTheme = {
+    name = "Papirus-Dark";
+    package = pkgs.papirus-icon-theme;
+  };
+
+  theme = {
+    name = "adw-gtk3";
+    package = pkgs.adw-gtk3;
+  };
+
+  gtk4.extraConfig = {
+    gtk-application-prefer-dark-theme = 1;
+  };
+
+  gtk3.extraConfig = {
+    gtk-application-prefer-dark-theme = 1;
+  };
+};
+ home.pointerCursor = {
+    name = "BreezeX-RosePine-Linux";
+    package = pkgs.rose-pine-cursor;
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
+  home.sessionVariables = {
+    XCURSOR_THEME = "BreezeX-RosePine-Linux";
+    XCURSOR_SIZE = "24";
+    QT_QPA_PLATFORMTHEME = "gtk";
+     QT_QPA_PLATFORM = "wayland";
+    SDL_VIDEODRIVER = "wayland";
+    XDG_SESSION_TYPE = "wayland";
+  };
+}
+
   # when a new Home Manager release introduces backwards
   # incompatible changes.
   #
