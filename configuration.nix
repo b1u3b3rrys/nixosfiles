d9345310eb71769fc23595aebfb350a39cb1c513
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 14:22:10 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.
-
 { config, pkgs, ... }:
 {
   imports =
@@ -8,59 +5,16 @@
       ./hardware-configuration.nix
 	./wmpackages.nix
 	./usrpackages.nix
+  ./
 	./misc.nix
 	./pipes.nix
 	./fonts.nix
 	./shell.nix
     ];
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-networking.hostName = "nixos"; # Define your hostname.
-home-manager = {
-	backupFileExtension = "hm-backup";
-};
-  # Enable networking
-  networking.networkmanager.enable = true;
-  # Set your time zone.
-  time.timeZone = "Europe/Bratislava";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "sk_SK.UTF-8";
-    LC_IDENTIFICATION = "sk_SK.UTF-8";
-    LC_MEASUREMENT = "sk_SK.UTF-8";
-    LC_MONETARY = "sk_SK.UTF-8";
-    LC_NAME = "sk_SK.UTF-8";
-    LC_NUMERIC = "sk_SK.UTF-8";
-    LC_PAPER = "sk_SK.UTF-8";
-    LC_TELEPHONE = "sk_SK.UTF-8";
-    LC_TIME = "sk_SK.UTF-8";
-  };
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us,ru";
-    variant = "";
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.b1u3rchie = {
-    isNormalUser = true;
-    description = "b1u3rchie";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [];
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   #experemental features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
   system.stateVersion = "26.05"; # Did you read the comment?
- 
 }
