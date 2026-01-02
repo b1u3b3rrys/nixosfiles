73c9701350d8ebe16ad433b4ed2d997aadfd3c62
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 18:34:52 +0200

    Update links.nix

--- home-manager/links.nix
+++ home-manager/links.nix
@@ -1,21 +1,23 @@
 { config, lib, pkgs, ... }:
 
 {
-  # === Niri Configuration ===
-  xdg.configFile."niri/75.jpg".source = ./75.jpg;
-  xdg.configFile."niri/config.kdl".source = ./niri/config.kdl;
+  xdg.configFile = {
+    # Niri Configuration
+    "niri/75.jpg".source = ./75.jpg;
+    "niri/config.kdl".source = ./niri/config.kdl;
 
-  # === Alacritty Configuration ===
-  xdg.configFile."alacritty/alacritty.toml".source = ./alacritty/alacritty.toml;
+    # Alacritty Configuration
+    "alacritty/alacritty.toml".source = ./alacritty/alacritty.toml;
 
-  # === Mako Configuration ===
-  xdg.configFile."mako/config".source = ./mako/config;
+    # Mako Configuration
+    "mako/config".source = ./mako/config;
 
-  # === Waybar Configuration ===
-  xdg.configFile."waybar/config.jsonc".source = ./waybar/config.jsonc;
-  xdg.configFile."waybar/style.css".source = ./waybar/style.css;
+    # Waybar Configuration
+    "waybar/config.jsonc".source = ./waybar/config.jsonc;
+    "waybar/style.css".source = ./waybar/style.css;
 
-  # === Fuzzel Configuration ===
-  xdg.configFile."fuzzel/powermenu.sh".source = ./fuzzel/powermenu.sh;
-  xdg.configFile."fuzzel/fuzzel.ini".source = ./fuzzel/fuzzel.ini;
+    # Fuzzel Configuration
+    "fuzzel/powermenu.sh".source = ./fuzzel/powermenu.sh;
+    "fuzzel/fuzzel.ini".source = ./fuzzel/fuzzel.ini;
+  };
 }
