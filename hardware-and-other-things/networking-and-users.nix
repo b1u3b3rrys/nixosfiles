d9904651f0f53fbac1bc9c347be9409a4377d448
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 14:28:37 +0200

    Update networking-and-users.nix

--- hardware-and-other-things/networking-and-users.nix
+++ hardware-and-other-things/networking-and-users.nix
@@ -37,4 +37,15 @@ home-manager = {
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
   };
+  programs.zsh = {
+   enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+oh-my-zsh = {
+      enable = true;
+      theme = "agnoster"; 
+    };
+};
+users.defaultUserShell = pkgs.zsh;
 }
