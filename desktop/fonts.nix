26a8583e686698c4edc7dfddb1b0392b24b9e7bd
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 18:40:41 +0200

    Update fonts.nix

--- desktop/fonts.nix
+++ desktop/fonts.nix
@@ -1,10 +1,14 @@
 { pkgs, ... }:
 {
+
+# Fonts, i cant say something about that config
 fonts.packages = with pkgs; [
+
   noto-fonts
   nerd-fonts.iosevka
   noto-fonts-cjk-sans
   noto-fonts-color-emoji
   liberation_ttf
+
 ];
 }
