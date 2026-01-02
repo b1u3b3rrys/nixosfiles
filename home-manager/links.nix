288540e3661ed662c0eb102aaa05b313b4dccf29
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 16:07:47 +0200

    Update links.nix

--- home-manager/links.nix
+++ home-manager/links.nix
@@ -1,1 +1,7 @@
 { config, lib, pkgs, ... }:
+{
+xdg.configFile."niri/75.jpg".source = ./75.jpg;
+xdg.configFile."fuzzel/fuzzel.ini".source = ./fuzzel/fuzzel.ini;
+xdg.configFile."niri/config.kdl".source = ./config.kdl;
+xdg.configFile."alacritty/alacritty.toml".source = ./alacritty/alacritty.toml
+xdg.configFile."fuzzel/powermenu.sh".source = ./fuzzel/powermenu.sh
