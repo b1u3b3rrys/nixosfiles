b5a08b48a1a16f28de2645e3ccfe3efce7b73f5a
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 18:17:51 +0200

    Create usrpkgs.nix

---
+++ desktop/usrpkgs.nix
@@ -0,0 +1,13 @@
+{ pkgs, config, ... }:
+{
+programs.steam.enable = true;
+environment.systemPackages = with pkgs; [
+	telegram-desktop
+	librewolf
+	cmatrix
+	prismlauncher
+	vesktop
+	cava
+	weechat
+];
+}
