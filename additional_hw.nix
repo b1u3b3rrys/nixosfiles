6489452c49758481d92665c11d40b0ba7c0b12df
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 14:07:00 +0200

    Create additional_hw.nix

---
+++ additional_hw.nix
@@ -0,0 +1,30 @@
+{ config, ... }:
+{
+
+
+
+
+
+
+
+
+
+	services.xserver.videoDrivers = ["nvidia"];
+
+	hardware.graphics = {
+	enable = true;
+	};
+hardware.nvidia = {
+
+	modesetting.enable = true;
+
+	powerManagement.enable = false;
+
+	powerManagement.finegrained = false;
+
+	open = true;
+
+	package = config.boot.kernelPackages.nvidiaPackages.beta;
+
+   };
+}
