36a531155f6721b1bdcce066d037fcab2b914f70
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 18:14:41 +0200

    Update usrpkgs.nix

--- desktop/usrpkgs.nix
+++ desktop/usrpkgs.nix
@@ -1,1 +1,43 @@
-.
+{ pkgs, lib, config, ...}:
+{
+xdg.icons.fallbackCursorThemes = [ "BreezeX-RosePine-Linux" ];
+programs.xwayland.enable = true;
+services.gvfs.enable = true;
+services.displayManager.ly.enable = true;
+xdg.portal = {
+	enable = true;
+	wlr.enable = true;
+};
+programs.nano.enable = false;
+  xdg.terminal-exec = {
+    enable = true;
+    settings = {
+      default = [
+        "alacritty.desktop"
+      ];
+    };
+  };
+programs.niri = {
+	enable = true;
+	useNautilus = true;
+};
+environment.systemPackages = with pkgs; [
+	swaybg
+  micro
+	git
+	nautilus
+	home-manager
+	gnome-themes-extra
+	xwayland-satellite
+	alacritty        	
+	mako
+	fuzzel
+	fastfetch
+	hyfetch
+	wl-clipboard
+	nwg-look
+	waybar
+	htop	
+];
+
+}
