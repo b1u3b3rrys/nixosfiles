bfa143e80ab8250451116192613ee69dd07e4102
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 18:39:34 +0200

    Update usrpkgs.nix

--- desktop/usrpkgs.nix
+++ desktop/usrpkgs.nix
@@ -1,13 +1,19 @@
 { pkgs, config, ... }:
 {
-programs.steam.enable = true;
-environment.systemPackages = with pkgs; [
-	telegram-desktop
-	librewolf
-	cmatrix
-	prismlauncher
-	vesktop
-	cava
-	weechat
-];
+
+  # Programs and Software Settings 
+  
+  # Enable Steam for gaming
+  programs.steam.enable = true;
+
+  # System Packages 
+  environment.systemPackages = with pkgs; [
+    telegram-desktop      # Messaging app
+    librewolf             # Privacy-focused browser
+    cmatrix               # Terminal matrix screensaver
+    prismlauncher         # Game launcher (presumably)
+    vesktop               # Virtual desktop software (if applicable)
+    cava                  # Console-based audio visualizer
+    weechat               # Command-line chat client
+  ];
 }
