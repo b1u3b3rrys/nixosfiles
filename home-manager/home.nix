1d5211b03518e3feff51f5091887ff9adfc6dd64
b1u3rchie <b1u3b3rrys@proton.me>
2026-01-02 18:45:00 +0200

    Update home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,52 +1,43 @@
 { config, pkgs, ... }:
+
 {
-imports = [
-   ./links.nix
-];
-environment.variables = {
-   		EDITOR = "micro";
-   };
-xdg.configFile."gtk-4.0/settings.ini".force = true;
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "b1u3rchie";
-  home.homeDirectory = "/home/b1u3rchie";
+  # Import External Config 
+  imports = [
+    ./links.nix
+  ];
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "26.05";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-  
-gtk = {
-  enable = true;
-
-  iconTheme = {
-    name = "Papirus-Dark";
-    package = pkgs.papirus-icon-theme;
+  # Environment Variables 
+  environment.variables = {
+    EDITOR = "micro";  # 
   };
 
-  theme = {
-    name = "adw-gtk3";
-    package = pkgs.adw-gtk3;
+  # GTK Configuration 
+  gtk = {
+    enable = true;
+
+    # GTK Icon Theme Settings
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    # GTK Theme Settings
+    theme = {
+      name = "adw-gtk3";
+      package = pkgs.adw-gtk3;
+    };
+
+    # GTK4 and GTK3 Dark Mode Preference
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
 
-  gtk4.extraConfig = {
-    gtk-application-prefer-dark-theme = 1;
-  };
-
-  gtk3.extraConfig = {
-    gtk-application-prefer-dark-theme = 1;
-  };
-};
- home.pointerCursor = {
+  # Cursor Settings 
+  home.pointerCursor = {
     name = "BreezeX-RosePine-Linux";
     package = pkgs.rose-pine-cursor;
     size = 24;
@@ -54,12 +45,27 @@ gtk = {
     x11.enable = true;
   };
 
+  # Home Manager Settings 
+  home.username = "b1u3rchie";  # Set username for Home Manager
+  home.homeDirectory = "/home/b1u3rchie";  # Set home directory
+
+  # Home Manager release version to ensure compatibility
+  home.stateVersion = "26.05";
+
+  # Enable Home Manager
+  programs.home-manager.enable = true;
+
+  # XDG Configurations 
+  # Force GTK settings to be applied
+  xdg.configFile."gtk-4.0/settings.ini".force = true;
+
+  # Session Variables 
   home.sessionVariables = {
-    XCURSOR_THEME = "BreezeX-RosePine-Linux";
-    XCURSOR_SIZE = "24";
-    QT_QPA_PLATFORMTHEME = "gtk4";
-     QT_QPA_PLATFORM = "wayland";
-    SDL_VIDEODRIVER = "wayland";
-    XDG_SESSION_TYPE = "wayland";
+    XCURSOR_THEME = "BreezeX-RosePine-Linux"; 
+    XCURSOR_SIZE = "24";  
+    QT_QPA_PLATFORMTHEME = "gtk4";  
+    QT_QPA_PLATFORM = "wayland";  
+    SDL_VIDEODRIVER = "wayland";  
+    XDG_SESSION_TYPE = "wayland";  
   };
 }
